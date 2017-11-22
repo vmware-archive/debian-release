cad2b7f94dd6d76d00692e4b80e9acd3238f099a
Goutam Tadi <gtadi@pivotal.io>
2017-11-22 09:15:58 -0800

    Refactor ppa release script
    Signed-off-by: Larry Hamel <lhamel@pivotal.io>

--- concourse/scripts/ppa-debian-release.bash
+++ concourse/scripts/ppa-debian-release.bash
@@ -2,38 +2,41 @@
 
 set -euo pipefail
 
+TEST_PPA="ppa:gpdb-test/db"
+DEBIAN_RELEASE_DIR="debian_release"
+GPDB_SRC_DIR="gpdb"
+
 gpg --import <(echo "$GPG_PRIVATE_KEY")
 
 set -x
 
 apt-get update
-apt-get install -y software-properties-common \
-                   debmake \
+apt-get install -y debmake \
                    equivs \
                    git
 
 # Regex to capture required gporca version and download gporca source
-ORCA_TAG=$(grep -Po 'v\d+.\d+.\d+' gpdb/depends/conanfile_orca.txt)
-git clone --branch ${ORCA_TAG} https://github.com/greenplum-db/gporca.git gpdb/gporca
+ORCA_TAG=$(grep -Po 'v\d+.\d+.\d+' ${GPDB_SRC_DIR}/depends/conanfile_orca.txt)
+git clone --branch ${ORCA_TAG} https://github.com/greenplum-db/gporca.git ${GPDB_SRC_DIR}/gporca
 
-mv debian-release/debian gpdb/
+mv ${DEBIAN_RELEASE_DIR}/debian ${GPDB_SRC_DIR}/
 
 # adapt Maintainer to correspond to the production or test account
-if [ ${PPA_REPO} == "ppa:gpdb-test/db" ]; then
-  sed -i 's/Maintainer: .*/Maintainer: GPBOSH (Testing GPDB) <gpbosh@pivotal.io>/' gpdb/debian/control
+if [ ${PPA_REPO} == ${TEST_PPA} ]; then
+  sed -i 's/Maintainer: .*/Maintainer: GPBOSH (Testing GPDB) <gpbosh@pivotal.io>/' ${GPDB_SRC_DIR}/debian/control
 fi
 
 # Create a changelog
 GPDB_VERSION=$(gpdb/getversion --short)
-pushd gpdb
+pushd ${GPDB_SRC_DIR}
     dch --create -M --package greenplum-db-oss -v ${GPDB_VERSION}  "${RELEASE_MESSAGE}"
     dch -M -r "ignored message"
 popd
 
-tar czf greenplum-db-oss_${GPDB_VERSION}.orig.tar.gz gpdb
+tar czf greenplum-db-oss_${GPDB_VERSION}.orig.tar.gz ${GPDB_SRC_DIR}
 
 # Generate source.changes file
-pushd gpdb
+pushd ${GPDB_SRC_DIR}
   debuild -S -sa
 popd
 
